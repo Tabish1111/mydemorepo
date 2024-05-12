7eed025687549b366dd20f95feb714e2fec2471e
“Tabish <tabishmukaddam18@gmail.com>
2024-05-12 11:16:16 +0530

    Version 3

--- my_prog.cpp
+++ my_prog.cpp
@@ -3,8 +3,9 @@
 int main(){
 
     int age=22;
-    std::cout << "Hello," << "I am "<< age << "years of age!";
+    std::cout << "Hello," << "I am "<< age << "years of age!\n";
 
+    std::cout << "Blah Blah!\n";
 
     return 0;
 }
